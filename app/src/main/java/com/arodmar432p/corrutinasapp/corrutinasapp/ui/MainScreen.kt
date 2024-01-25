057bb7f7d2c0e77afe0f721210edf94d8abf43b7
Agustrodmar <agustrodmar@gmail.com>
2024-01-25 10:44:23 +0100

    Finished parte1

--- app/src/main/java/com/arodmar432p/corrutinasapp/corrutinasapp/ui/MainScreen.kt
+++ app/src/main/java/com/arodmar432p/corrutinasapp/corrutinasapp/ui/MainScreen.kt
@@ -7,33 +7,26 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 
 
 @Composable
 fun MainScreen(viewModel: MainViewModel) {
-    var color by remember { mutableStateOf(Color.Red) }
-
     Box(
         contentAlignment = Alignment.Center,
         modifier = Modifier.fillMaxSize()
     ) {
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
             Button(
-                onClick = { color = if (color == Color.Red) Color.Blue else Color.Red },
-                colors = ButtonDefaults.buttonColors(containerColor = color)
+                onClick = { viewModel.changeColor() },
+                colors = ButtonDefaults.buttonColors(containerColor = viewModel.color)
             ) {
                 Text("Cambiar color")
             }
 
-            Text(viewModel.apiResponse.value)
+            Text(viewModel.apiResponse)
 
             Button(onClick = { viewModel.callApi() }) {
                 Text("Llamar API")
--- app/src/main/java/com/arodmar432p/corrutinasapp/corrutinasapp/ui/MainViewModel.kt
+++ app/src/main/java/com/arodmar432p/corrutinasapp/corrutinasapp/ui/MainViewModel.kt
@@ -1,13 +1,21 @@
 package com.arodmar432p.corrutinasapp.corrutinasapp.ui
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.ViewModel
 import com.arodmar432p.corrutinasapp.corrutinasapp.domain.LockUseCase
 
 class MainViewModel(private val lockUseCase: LockUseCase) : ViewModel() {
-    val apiResponse = mutableStateOf("")
+    var apiResponse by mutableStateOf("")
+    var color by mutableStateOf(Color.Red)
 
     fun callApi() {
-        apiResponse.value = lockUseCase.bloqueoApp()
+        apiResponse = lockUseCase.bloqueoApp()
+    }
+
+    fun changeColor() {
+        color = if (color == Color.Red) Color.Blue else Color.Red
     }
 }
